d432e731000f1221b63760c6120bd493fbcbc8ef
Cathie Yun <cath_yun@mit.edu>
2022-03-20 23:19:54 -0700

    clean up k-hot direct benches

--- benches/k_hot_direct.rs
+++ benches/k_hot_direct.rs
@@ -13,21 +13,21 @@ use merlin::Transcript;
 use bulletproofs::KHotProof;
 use bulletproofs::{BulletproofGens, PedersenGens};
 
-/// Run with `cargo bench k_hot_direct --features="yoloproofs"`
+/// Run with `cargo bench --bench=k_hot_direct --features="yoloproofs"`
 /// Different k-hot vector lengths to try
 static TEST_SIZES: [usize; 6] = [1, 2, 32, 64, 1024, 16384];
 
-fn create_k_hot_direct_helper(n: usize, c: &mut Criterion) {
+fn create_k_hot_direct_helper(c: &mut Criterion) {
     let k = 1;
-    let label = format!("{}-bit {}-hot proof creation", n, k);
+    let label = format!("{}-hot proof creation", k);
 
     c.bench_function_over_inputs(
         &label,
-        move |b, _l| {
+        move |b, n| {
             let pc_gens = PedersenGens::default();
-            let bp_gens = BulletproofGens::new(n, 1);
+            let bp_gens = BulletproofGens::new(*n, 1);
 
-            let mut secret_vec = vec![0; n];
+            let mut secret_vec = vec![0; *n];
             // TODO: choose index randomly
             secret_vec[n-1] = 1;
 
@@ -47,46 +47,20 @@ fn create_k_hot_direct_helper(n: usize, c: &mut Criterion) {
     );
 }
 
-fn create_k_hot_direct_n_8(c: &mut Criterion) {
-    create_k_hot_direct_helper(8, c);
-}
-
-fn create_k_hot_direct_n_16(c: &mut Criterion) {
-    create_k_hot_direct_helper(16, c);
-}
-
-fn create_k_hot_direct_n_32(c: &mut Criterion) {
-    create_k_hot_direct_helper(32, c);
-}
-
-fn create_k_hot_direct_n_64(c: &mut Criterion) {
-    create_k_hot_direct_helper(64, c);
-}
-
-fn create_k_hot_direct_n_131072(c: &mut Criterion) {
-    create_k_hot_direct_helper(131072, c);
-}
-
-fn create_k_hot_direct_n_524288(c: &mut Criterion) {
-    create_k_hot_direct_helper(524288, c);
-}
-
-fn create_k_hot_direct_n_1048576(c: &mut Criterion) {
-    create_k_hot_direct_helper(1048576, c);
-}
-
-fn verify_k_hot_direct_helper(n: usize, c: &mut Criterion) {
+fn verify_k_hot_direct_helper(c: &mut Criterion) {
     let k = 1;
-    let label = format!("{}-bit {}-hot proof verification", n, k);
+    let label = format!("{}-hot proof verification", k);
     let pc_gens = PedersenGens::default();
-    let bp_gens = BulletproofGens::new(n, 1);
+
+    // Make a generator that will accomodate the largest test case
+    let max_test_size = TEST_SIZES.iter().max().unwrap();
+    let bp_gens = BulletproofGens::new(*max_test_size, 1);
 
     c.bench_function_over_inputs(
         &label,
-        move |b, _l| {
+        move |b, n| {
             let proof = {
-
-                let mut secret_vec = vec![0; n];
+                let mut secret_vec = vec![0; *n];
                 // TODO: choose index randomly
                 secret_vec[0] = 1;
 
@@ -106,65 +80,25 @@ fn verify_k_hot_direct_helper(n: usize, c: &mut Criterion) {
                 // Each proof creation requires a clean transcript.
                 let mut transcript = Transcript::new(b"KHotDirectBenchmark");
 
-                proof.verify(&bp_gens, &pc_gens, &mut transcript, n)
+                proof.verify(&bp_gens, &pc_gens, &mut transcript, *n)
             });
         },
         TEST_SIZES,
     );
 }
 
-fn verify_k_hot_direct_n_8(c: &mut Criterion) {
-    verify_k_hot_direct_helper(8, c);
-}
-
-fn verify_k_hot_direct_n_16(c: &mut Criterion) {
-    verify_k_hot_direct_helper(16, c);
-}
-
-fn verify_k_hot_direct_n_32(c: &mut Criterion) {
-    verify_k_hot_direct_helper(32, c);
-}
-
-fn verify_k_hot_direct_n_64(c: &mut Criterion) {
-    verify_k_hot_direct_helper(64, c);
-}
-
-fn verify_k_hot_direct_n_131072(c: &mut Criterion) {
-    verify_k_hot_direct_helper(131072, c);
-}
-
-fn verify_k_hot_direct_n_524288(c: &mut Criterion) {
-    verify_k_hot_direct_helper(524288, c);
-}
-
-fn verify_k_hot_direct_n_1048576(c: &mut Criterion) {
-    verify_k_hot_direct_helper(1048576, c);
-}
-
 criterion_group! {
     name = create_k_hot_direct;
     config = Criterion::default().sample_size(10);
     targets =
-    create_k_hot_direct_n_8,
-    create_k_hot_direct_n_16,
-    create_k_hot_direct_n_32,
-    create_k_hot_direct_n_64,
-    // create_k_hot_direct_n_131072,
-    // create_k_hot_direct_n_524288,
-    // create_k_hot_direct_n_1048576,
+    create_k_hot_direct_helper,
 }
 
 criterion_group! {
     name = verify_k_hot_direct;
     config = Criterion::default().sample_size(10);
     targets =
-    verify_k_hot_direct_n_8,
-    verify_k_hot_direct_n_16,
-    verify_k_hot_direct_n_32,
-    verify_k_hot_direct_n_64,
-    // verify_k_hot_direct_n_131072,
-    // verify_k_hot_direct_n_524288,
-    // verify_k_hot_direct_n_1048576,
+    verify_k_hot_direct_helper,
 }
 
 criterion_main!(create_k_hot_direct, verify_k_hot_direct);
